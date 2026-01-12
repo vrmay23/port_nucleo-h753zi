232921b8d5d729fbe5ae78fb24ba28a3a836406a
Tobias Gustafsson <tg.tobias@gmail.com>
2026-01-12 18:50:01 +0100

    Fix for ext CAN msg

--- nuttx/arch/arm/src/stm32h7/stm32_fdcan_sock.c
+++ nuttx/arch/arm/src/stm32h7/stm32_fdcan_sock.c
@@ -2269,7 +2269,7 @@ int fdcan_initialize(struct fdcan_driver_s *priv)
    * https://community.st.com/s/question/0D73W000001nzqFSAQ
    */
 
-  const uint8_t n_extid = 64;
+  const uint8_t n_extid = 128;
   priv->message_ram.filt_extid_addr = gl_ram_base + ram_offset * WORD_LENGTH;
 
   regval = (n_extid << FDCAN_XIDFC_LSE_SHIFT) & FDCAN_XIDFC_LSE_MASK;
